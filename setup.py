ed2517f69e939c112a69475600e1b8e3f1fb0871
alextimakov <alextimakoff94@gmail.com>
2019-10-27 19:53:09 +0300

    version 0.0.1

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ds_comp',  # Required
+    version='0.0.1',  # Required
+    description='Competencies app for Data Science team',  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    python_requires='>=3.5, <4',  # Required
+    install_requires=['dash', 'dash-daq', 'pandas', 'numpy', 'plotly', 'pymongo', 'requests', 'flake8'],  # Optional
+    entry_points={  # Optional
+        'console_scripts': [
+            'ds_comp=ds_comp:app',
+        ],
+    }
+)
